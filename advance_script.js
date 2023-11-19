25dd126305ea3534d4a0ea9c73d69cf468204262
Tejas <tejaspatiltp.1999@gmail.com>
2023-11-19 23:27:43 +0530

    Frontend Basics

---
+++ advance_script.js
@@ -0,0 +1,201 @@
+/*
+-var let const
+--the difference
+*/
+
+// javascript 
+// es5(old) ->  var
+// es6(new) version -> const let
+// We are using es5 + es6 (when web browser gets update it also update new properties of (html , css , js) preinstalled in browser)
+
+
+// var old js mein tha
+
+// var function scoped hota hai -> var apne parent func me kahi bhi use ho skta hai
+function abcd() {
+    for(var i = 0 ; i < 5 ; i++) {
+        console.log(i); // 0 1 2 3 4
+    }
+    console.log(i); // 5 
+}
+abcd();
+
+// var add itself to the window object
+    // -> js me kahi saare features hai par kuch features joh hum use krte hai voh nhi hai
+    // par phir bhi use kr paate hai kyuki wo features js language use kar leti hai window object se 
+    // (window is a box of features given by browser to use)
+    // alert prompt console (part of window object)
+
+    var a = 10; // expose variable name on browser
+ 
+
+// let const new js mein hai
+// let braces scoped hota hai
+
+function abcd() {
+    for(let i = 0 ; i < 5 ; i++) {
+        console.log(i); // 0 1 2 3 4
+    }
+    // console.log(i); // error
+}
+abcd();
+
+// let doesn't add itself to the window object
+
+let b = 10;
+
+
+// execution context is a container where the function code is executed and it is created when function is called.
+// it contains 3 things, variables , function and lexical environment of that function.
+
+// lexical environment hota hai ek chart jisme likha hota hai apka particular function kin cheejon ko access 
+// kar skta hai aur kinko nhi. ex. abc() can access a and dummy() but not b.
+// it holds function scope and it's scope chain.
+
+function abc(){
+    var a = 10;
+    function dummy(){
+        var b = 12;
+    }
+}
+
+abc(); // func abc() can access var a and func dummy() but not var b (cos var has scope inside the parent func i.e. dummy)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// how to copy reference value
+
+var m = [ 1 , 2 , 3 , 4 , 5];
+var n = m; // n is referring to array m memory location
+
+n.pop();
+console.log(m); // [ 1, 2, 3, 4 ]
+console.log(n); // [ 1, 2, 3, 4 ]
+
+var x = [ 1 , 2 , 3 , 4 , 5];
+var y = [...x]; //spread operator copies array x values and repace in place of spread operator
+                //~~~~~~
+
+y.pop();
+console.log(x); // [ 1, 2, 3, 4, 5 ]
+console.log(y); // [ 1, 2, 3, 4] // y is not referring to array x memory location
+
+
+// copy reference value in obj
+
+var obj = {name:"Tejas"};
+var copyobj = obj;
+
+obj.name = "Hello";
+console.log(obj); // { name: 'Hello' }
+console.log(copyobj); // { name: 'Hello' }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var obj = {name:"Tejas"};
+var copyobj = {...obj}; // spread operator copies object of obj to copyobj
+
+obj.name = "Hello";
+console.log(obj); // { name: 'Hello' }
+console.log(copyobj); // { name: 'Tejas' }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Truthy and Falsy
+//js me kuch bhi likho 2 types k hote hai Truthy and Falsy
+
+//Falsy values -> 0 , false , undefined null , NaN , document.all
+
+if(0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+//false
+
+if(-1) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+//true
+
+if("Tejas") {
+    console.log("true");
+} else {
+    console.log("false");
+}
+//true
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+switch(2) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    default:
+        console.log("Not a valid Input");
+} // Tuesday
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// forEach loop only works on array
+
+var arr = [10 , 20 , 30 , 40 , 50];
+arr.forEach(function(val){
+    console.log(val);
+});
+// o/p
+// 10
+// 20
+// 30
+// 40
+// 50
+
+var arr = [10 , 20 , 30 , 40 , 50];
+arr.forEach(function(val){
+    console.log(val + 2);
+});
+
+// forEach will not make change in original array it changes array tmeporary copy , original array will remain same.
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// forin loop
+// objects par loop karne k liye forin loop use hota hai
+
+var obj = {
+    name : "Tejas",
+    age : 23,
+    city : "pune"
+}
+
+for(var key in obj) {
+    console.log(key);
+}
+// o/p
+// name
+// age
+// city
+
+for(var key in obj) {
+    console.log(key , obj[key]);
+}
+// o/p
+// name Tejas
+// age 23
+// city pune
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var a = 12;
+
+do{
+    console.log(a);
+    a++
+} while(a > 15); // condition is false
+
+// o/p -> 12
---
+++ script.js
@@ -0,0 +1,162 @@
+/*
+JS
+-word vs keyword
+-var let const
+--the difference
+--window object (set of features given by browser)
+--browser context api (includes window obj , heap and stack)
+--stack ( first in last out)
+--heap memory -> memory where data is stored
+--execution context 
+--lexical environment
+-hoisting
+-types in js
+--how to copy reference value
+primitive and reference
+conditionals // if else else-if 
+--truthy vs falsy
+loops // for and while
+--foreach forin  do-while
+function
+arrays
+push pop shift unshift splice
+object
+*/
+
+// word vs keyword ( word ko compiler nhi samjata aur keyword ko compiler smjta hai)
+//chacha -> word
+// for -> keyword
+
+
+//var const let
+//variable and constant
+
+// code me kuch bhi data store krna hai toh variable use hota hai (variable matlab value badle)
+// constant (value badalta nahi hai)
+
+const a = 10;
+var b = 20;
+console.log(a + b); // 30
+// a = 20; // const value cannot be change
+b = 30;
+console.log(a + b); // 40
+
+
+//hoisting - variable and function are hoisted which means there declaration is moved on the top of the code
+
+var c = 10;
+
+var c; // js will covert above line in this and declaration will move to the top of the code
+c = 10;
+
+var d; // default value is undefined
+console.log(d); // undefined (cos of hoisting)
+d = 10;
+
+// undefined vs not defined
+// when var is declared and try printing then undefined
+// when var is not declare and try printing than not defined
+
+
+//primitives and reference
+
+
+//primitives = number , string
+// reference = [] () {}
+// aesi kohi bhi value jisko copy krne par real copy nhi hota , balki us main value ka reference pass hojaata hai, 
+//Aur jiska copy krne par real copy ho jaaye wo value primitive type value hai
+
+
+var m = 10;
+var n = m;
+ n += 2;
+ console.log(n); // 12
+
+ var arr = [12 ,13];
+ var arr1 = arr;
+
+ arr1.pop();
+ console.log(arr1);//[ 12 ]
+ console.log(arr);// [ 12 ] // cos arr1 is refering to arr ka array
+
+
+ //conditionals // if else else-if 
+ 
+ if(11>11){
+
+ } else if(12 > 11) {
+
+ } else {
+
+ }
+
+
+ //loops  (matlab repeat) // for and while
+
+ for(var i = 0 ; i <= 10 ; i++) {
+    console.log(i);
+ }
+
+ var i = 0;
+
+ while(i < 11) {
+   console.log(i);
+   i++;
+ }
+
+
+ // function is use for 3 things
+ // 1. when I want to run my code not now but in future
+ // 2. for code reuse
+ // 3. when I want to run my code with different parameters
+
+function sayHello(){
+   console.log("hello");
+}
+
+sayHello();
+
+
+function abcd(a , b , c){ // parameters (variables in which values of arguments are stored)
+   console.log(a);
+}
+
+abcd(12, 13, 14); // arguments (real value that is give while calling func)
+
+// arrays => to store multiple value in single variable
+//arrays are group of values
+
+var arr = [11 , 12 ,13 , 14 , 15];
+arr.push(19); // [11 , 12 ,13 , 14 , 15 , 19] // add value at the end
+arr.pop(); // [11 , 12 ,13 , 14 , 15] // remove value from end
+arr.pop(); // [11 , 12 ,13 , 14]
+
+arr.unshift(0); // [0 ,11 , 12 ,13 , 14] // add value at begining of arr
+
+arr.shift() //[11 , 12 ,13 , 14] // remove value from begining of arr
+arr.shift() //[12 ,13 , 14]
+
+arr.splice(1 , 1); // remove value from between of arr (konse index se , kitne value starting from index)
+
+console.log(arr); //[ 12, 14 ]
+
+
+// objects
+// ek bande k details ko hold krna in key-value pair
+
+var ghadi = {
+   brand: "Kenneth Cole", // property
+   price: "16k",
+   color: "silver",
+   type: "automatic",
+   digital: false,       // property
+
+   sayHi : function(){console.log("Hi");} // method
+}
+
+console.log(ghadi.type); //automatic
+console.log(ghadi.sayHi()); // Hi
+
+
+ghadi.brand = "Titan Kenneth Cole";
+console.log(ghadi);
